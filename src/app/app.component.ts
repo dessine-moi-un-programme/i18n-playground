8b6671e204e666239ca31d6359321dd64bba583e
dessinemoiunprogramme <dessinemoiunprogramme@gmail.com>
2021-05-04 22:34:24 +0200

    Mise en place i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'i18n-playground';
   nextArrivalDate: Date = new Date();
   price: number = 1.5;
   percentage: number = 0.7;
   pineappleNumber: number = 2;
+
+  constructor(@Inject(LOCALE_ID) public language: string) { }
+
+  languageChanged(): void {
+    window.location.href = location.origin + '/' + this.language + '/index.html';
+  }
 }
